39237b9360103fe8717c27ce48452a406ae3a4d6
Dominik Wojnowski <d.wojnowski.priv@gmail.com>
2024-03-04 20:37:27 +0100

    added methods: transfer, get_balance, withdraw

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 class Category:
-    def __init__(self):
+    def __init__(self, category_name):
+        self.name = category_name
         self.ledger = []
 
-    def deposit(self, amount, description):
+    def deposit(self, amount: float, description: str) -> None:
         self.ledger.append({"amount": amount, "description": description})
+
+    def withdraw(self, amount: float, description: str) -> None:
+        self.ledger.append({"amount": -amount, "description": description})
+
+    def get_balance(self) -> float:
+        return sum([i['amount'] for i in self.ledger])
+
+    def transfer(self, amount:float, category_obj) -> None:
+        self.withdraw(amount, f'Transfer to {category_obj.name}')
+        category_obj.deposit(amount, f'Transfer from {self.name}')
+
 
 def create_spend_chart(categories):
     pass
+
+
