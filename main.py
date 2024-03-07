7325dcb65acdd964ab5616e23c8dc128bde60f0e
Dominik Wojnowski <d.wojnowski.priv@gmail.com>
2024-03-07 21:22:53 +0100

    updated create_spend_chart function

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class Category:
             return False
 
     def check_funds(self, amount) -> bool:
-        return True if self.get_balance() > amount else False
+        return True if self.get_balance() >= amount else False
 
     def get_balance(self) -> float:
         return sum([i['amount'] for i in self.ledger])
@@ -36,9 +36,57 @@ class Category:
         out += f'\nTotal: {self.get_balance()}'
         return out
 
+from collections import defaultdict
+def create_spend_chart(categories: [Category]) -> None:
+    # spent_by_category_value = [0] * len(categories)
+    spent_by_category_value = defaultdict(int)
+    for i, categorie in enumerate(categories):
+        for ledger in categorie.ledger:
+            amount = ledger['amount']
+            if amount < 0:
+                # spent_by_category_value[i] -= amount
+                spent_by_category_value[categorie.name] -= amount
 
-def create_spend_chart(categories):
-    pass
+    print(spent_by_category_value)
+    # get max
+    sum_value = sum([v for v in spent_by_category_value.values()])
+    print(sum_value)
+    for key in spent_by_category_value:
+        value = spent_by_category_value[key]
+        spent_by_category_value[key] = round((value / sum_value) * 10) * 10
+    print(spent_by_category_value)
+    out = 'Percentage spent by category'
+
+    for i in range(11):
+        str_value = f'{str((10 - i) * 10)}|'
+        out += f'\n{str_value:>4}'
+    out += '\n    ' + '-' * 3 * len(categories) + '-'
+
+
+    print(out)
+
+
+# Percentage spent by category
+# 100|
+#  90|
+#  80|
+#  70|
+#  60| o
+#  50| o
+#  40| o
+#  30| o
+#  20| o  o
+#  10| o  o  o
+#   0| o  o  o
+#     ----------
+#      F  C  A
+#      o  l  u
+#      o  o  t
+#      d  t  o
+#         h
+#         i
+#         n
+#         g
 
 
 food = Category("Food")
@@ -47,4 +95,7 @@ food.withdraw(10.15, "groceries")
 food.withdraw(15.89, "restaurant and more food for dessert")
 clothing = Category("Clothing")
 food.transfer(50, clothing)
+clothing.withdraw(13.4,"qaws")
 print(food)
+
+create_spend_chart([food , clothing])
